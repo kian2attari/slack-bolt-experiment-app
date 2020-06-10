bec3b88e564112a2c7c879ad4729a321b465cacb
Kian Attari <kattari@slack-corp.com>
2020-06-10 18:47:19 -0400

    added webhook route

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const { App } = require('@slack/bolt');
+const { App, ExpressReceiver } = require('@slack/bolt');
+const express  = require('express');
 
-// Initializes your app with your bot token and signing secret
+
+// Create a Bolt Receiver
+const expressReceiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
+
+// Initializes your app with your bot token, signing secret, and receiver
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  receiver: expressReceiver
 });
+
 
 // Listens to incoming messages that contain 'yo bot'
 app.message('yo bot', async ({ message, say}) => {
@@ -41,9 +47,31 @@ app.action('button_click', async ({body, ack, say}) => {
   await say(`<@${body.user.id}>, thanks for clicking my button bro. That's respect :100:`);
 });
 
+// Parsing JSON Middleware?
+
+expressReceiver.router.use(express.json())
+
+
+// Receive github webhooks here!
+expressReceiver.router.post('/webhook', (req, res) => {
+  // You're working with an express req and res now.
+  var github_event_issue_body = req.body;
+
+  console.log(github_event_issue_body);
+
+  res.send('Webhook received successfully yeehaw');
+});
+
+
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
   console.log('⚡️ Bolt app is running!');
 })();
+
+
+
+
+
+
