aee332964c608caf7dade3cf8e2fc436a3201af7
Kian Attari <kattari@slack-corp.com>
2020-06-16 19:31:36 -0400

    auth attempt

--- graphql/gh-graphql.js
+++ graphql/gh-graphql.js
@@ -2,22 +2,41 @@ const { createAppAuth } = require("@octokit/auth-app");
 const { graphql } = require("@octokit/graphql");
 
 
+// async function jwt() {
+//   const { token } = await githubAppJwt({
+//     id: process.env.APP_ID,
+//     privateKey: process.env.PRIVATE_KEY
+//   });
+//   return token;
+// }
+
+// console.log(jwt())
+
+
+
+
 // TODO: Installation ID
 const auth = createAppAuth({
   id: process.env.APP_ID,
   privateKey: process.env.PRIVATE_KEY,
-  installationId: 9783180,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
 });
+
+
+
 
 const graphqlWithAuth = graphql.defaults({
   request: {
-    hook: auth.hook,
-  },
+    hook: auth.hook
+  }
 });
+
 
 
 const call_gh_graphql = async function call_gh_graphql_api(query, variables) {
     try {
+
 
         const data = await graphqlWithAuth(query, variables);
 
