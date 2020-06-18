065b59949889649cda000a881a7470a52f971abe
Kian Attari <kattari@slack-corp.com>
2020-06-18 15:48:28 -0400

    apphome edit

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ app.event('app_mention', async ({ event, context }) => {
 app.event('app_home_opened', async ({ event, context, client }) => {
   try {
     console.log(event); 
-
+    console.log(AppHome)
     /* view.publish is the method that your app uses to push a view to the Home tab */
     const result = await client.views.publish({
 
