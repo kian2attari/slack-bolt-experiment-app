a0ca0765e96091c1c87ae3603ea89f08f3d96d25
Kian Attari <kattari@slack-corp.com>
2020-08-11 14:19:04 -0400

    renamed modules

--- listeners/views/index.js
+++ listeners/views/index.js
@@ -1,10 +1,10 @@
 const {
   edit_triage_duty_availability_modal,
-} = require('./editTriageDutyAvailabilityModal');
+} = require('./editTriageDutyAvailabilityModalView');
 
 module.exports = {
-  setup_triage_team_view: require('./setupTriageWorkflow'),
-  map_username_modal_view: require('./mapUsernameModal'),
-  setup_org_project_modal_view: require('./setupOrgProjectModal'),
+  setup_triage_team_view: require('./setupTriageWorkflowView'),
+  map_username_modal_view: require('./mapUsernameModalView'),
+  setup_org_project_modal_view: require('./setupOrgProjectModalView'),
   edit_triage_duty_availability_modal,
 };
