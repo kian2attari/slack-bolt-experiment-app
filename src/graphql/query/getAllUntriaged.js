c845887a740ead9a716eb1b5d2d38ba9d6d4503c
Kian Attari <kattari@slack-corp.com>
2020-08-20 19:37:43 -0400

    limit on number of cards

--- src/graphql/query/getAllUntriaged.js
+++ src/graphql/query/getAllUntriaged.js
@@ -1,12 +1,14 @@
 /* Gets all the untriaged cards associated with the project input. It also returns all the triage labels of the repo that 
 the issues come from. Triage labels are identified by having MAIN-TRIAGE: at the beginning of their label descriptions */
+/* The limit of 7 cards is arbitrary to reduce the clutter (by just showing the 7 latest cards), but it can't be too high 
+because each card is represented by a bunch of blocks on the App Home, and there is a limit to how many blocks you can have on a page. */
 module.exports = `
 query getAllUntriaged($projectIds: [ID!]!) {
   nodes(ids: $projectIds) {
     ... on Project {
       id
       name
-      pendingCards(first: 20) {
+      pendingCards(first: 7) {
         totalCount
         nodes {
           id
